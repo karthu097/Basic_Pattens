2652a49dbf742167f3d45a89c23885a46d26ce98
karthu097 <chandrapatikarthikeya2006@gmail.com>
2026-04-10 23:52:03 +0530

    Max number in the array

--- Basic_Pattens-main/BasicArrayPattens.java
+++ Basic_Pattens-main/BasicArrayPattens.java
@@ -1,2 +1,14 @@
 public class BasicArrayPattens {
+    public static void main(String[] args){
+        int arr[] ={32, 67, 23, 89, 45, 12, 78};
+        int max = arr[0]; // assume first index is the biggest
+        for(int i =0 ;i<arr.length;i++){
+            if(arr[i] > max){ //if  you found soething bigger
+                max = arr[i]; //then update it
+            }
+        }
+        System.out.println(max);
+    }
+
 }
+/*  assume that */
