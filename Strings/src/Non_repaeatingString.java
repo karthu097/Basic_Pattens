594da961976a845f1db979320a5c03a032fad89b
karthu097 <chandrapatikarthikeya2006@gmail.com>
2026-04-11 17:41:37 +0530

    Strings

---
+++ Strings/src/Non_repaeatingString.java
@@ -0,0 +1,5 @@
+public class Non_repaeatingString {
+    public static void main(String[] args){
+
+    }
+}
--- Strings/src/String_problem2.java
+++ Strings/src/String_problem2.java
@@ -2,7 +2,7 @@ public class String_problem2 {
     public static void main(String[] args){
         String str = "Hello";
         for(int i = str.length() -1; i >=0 ; i--){
-            System.out.println(str.charAt(i));
+            System.out.println("at the index " +i +": "+str.charAt(i));
         }
     }
 }
