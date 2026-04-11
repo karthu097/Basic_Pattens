7a339868c63bcb8cec48fffb207e300e0a85353a
karthu097 <chandrapatikarthikeya2006@gmail.com>
2026-04-11 12:09:59 +0530

    Strings

---
+++ Basic_Pattens-main/FindMin_Avg.java
@@ -0,0 +1,21 @@
+public class FindMin_Avg {
+    public static void main(String[] args){
+        int arr[] = {34, 67, 23, 89, 45, 12, 78};
+        int min = arr[0];
+        int count =0 ;
+        double  sum = 0;
+        for(int i =0;i<arr.length;i++){
+            if(arr[i]<min){
+                min = arr[i];
+            }
+            count++;//double avg = sum / arr.length; here no need to write count you can write directly array length
+            sum = sum + arr[i];
+
+        }
+        double avg = (sum)/count;
+
+        System.out.println("the average of the array :" + avg);
+
+        System.out.println("Minimumm number : " + min);
+    }
+}
---
+++ Strings/src/String_problem1.java
@@ -0,0 +1,8 @@
+public class String_problem1{
+    public static void main(String[] args){
+        String str = "Hello";
+        for(int i =0;i<str.length();i++){
+            System.out.println(str.charAt(i));
+        }
+    }
+}
