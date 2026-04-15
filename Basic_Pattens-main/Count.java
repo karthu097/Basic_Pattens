e28ca0b6f10e8ce4df8bba4900aa5e86e0158530
karthu097 <chandrapatikarthikeya2006@gmail.com>
2026-04-15 21:58:06 +0530

    Count the number of even integers

--- Basic_Pattens-main/Count.java
+++ Basic_Pattens-main/Count.java
@@ -5,11 +5,11 @@ public class Count {
         System.out.println("Enter any number ");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int count = 0;
+        int count = 0;//here we are using the count variable to count the no . of times the loop is repeating
 
         for (int i = 0; i <= n; i++) {
             System.out.println(i);
-            if(i%2==0){
+            if(i%2==0){ // this is to  check the given number is even , if it is even then it will count .
                 System.out.print("Even");
 
             }
