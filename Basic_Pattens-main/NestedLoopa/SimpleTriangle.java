77fa7d770ac2be07af81c97e55e5d1fc5e14b303
karthu097 <chandrapatikarthikeya2006@gmail.com>
2026-04-25 23:10:23 +0530

    Simple Triangle

--- Basic_Pattens-main/NestedLoopa/SimpleTriangle.java
+++ Basic_Pattens-main/NestedLoopa/SimpleTriangle.java
@@ -1,4 +1,25 @@
 package NestedLoopa;
 
 public class SimpleTriangle {
+    public static void main(String[] args){
+        int rows = 3;
+        int clos = 6;
+        for(int i =1;i<=rows;i++) {
+            for (int j = 1; j <= clos; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+        class Triangle  {
+            int rows = 5;
+            for(int  i =1;i<=rows;i++){
+                for(int j = 1;j<=i;j++){
+                    System.out.println( );
+                }
+            }
+
+        }
+
+    }
 }
