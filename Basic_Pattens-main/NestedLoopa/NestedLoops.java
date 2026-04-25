19ffd24c2c12feedcfabf7ee1f0b68e0f7527729
karthu097 <chandrapatikarthikeya2006@gmail.com>
2026-04-25 23:09:09 +0530

    Nested Loops

--- Basic_Pattens-main/NestedLoopa/NestedLoops.java
+++ Basic_Pattens-main/NestedLoopa/NestedLoops.java
@@ -1,4 +1,12 @@
 package NestedLoopa;
-
 public class NestedLoops {
+    public static void main(String[] args){
+        for(int i = 1; i <= 2; i++){
+            for(int j = 1; j <= 3; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
 }
