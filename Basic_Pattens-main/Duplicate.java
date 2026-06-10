d1d1df4281c7dfe114981ece69f43cc7dbbceb81
Karthu097 <chandrapatiKarthikeya2006@gmail.com>
2026-06-10 22:57:22 +0530

    brute force vs hashset

--- Basic_Pattens-main/Duplicate.java
+++ Basic_Pattens-main/Duplicate.java
@@ -1,15 +1,30 @@
 import java.util.HashSet;
 
 public class Duplicate {
-    public static void main(String[] args){
-        int[] arr ={1,2,3,2,5,7};
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 2, 5, 7};
         HashSet<Integer> set = new HashSet<>();
-        for(int num : arr){
-            if(set.contains(num)) {
+        //using hash set
+        for (int num : arr) {
+            if (set.contains(num)) {
                 System.out.println("Duplicate: " + num);
-            }else{
+            } else {
                 set.add(num);
+                }
+            }
+        //brute force
+                for (int i = 0; i < arr.length; i++) {
+                    for (int j = i + 1; j < arr.length; j++) {
+                        if (arr[i] == arr[j]) {
+                            System.out.println("using brute force :"+arr[i]);
+                        }
+
+
+                    }
+                }
             }
         }
-    }
-}
+
+
+
+
