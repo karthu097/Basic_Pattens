ed9a4a444d0f72222d080e7b85602b0ea99fef3b
Karthu097 <chandrapatiKarthikeya2006@gmail.com>
2026-06-13 19:23:24 +0530

    finding the duplicate using the brute force method

--- Basic_Pattens-main/Day_8/Duplicates.java
+++ Basic_Pattens-main/Day_8/Duplicates.java
@@ -4,13 +4,15 @@ import java.util.*;
 public class Duplicates {
     public static void main(String[] args){
         int arr[] ={4,5,7,9,4,33};
-         HashSet<Integer> set = new HashSet<>();
-         for(int i =0;i<arr.length;i++){
-             if(set.contains(arr[i])){
-                 System.out.println(arr[i] + " is  the duplicate in the array  ");
-             }else{
-                 set.add(arr[i]);
-             }
-         }
+        for(int i=0; i<arr.length; i++)
+        {
+            for(int j=i+1; j<arr.length; j++)
+            {
+                if(arr[i] == arr[j])
+                {
+                    System.out.println(arr[i] + " duplicate");
+                }
+            }
+        }
         }
 }
